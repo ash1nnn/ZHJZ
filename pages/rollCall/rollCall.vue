edcc0a99d610ac3a401fa0b8f1cb6b7810c1d112
ash1nnn <lczlalala@gmail.com>
2023-07-05 23:24:04 +0800

    666

--- pages/rollCall/rollCall.vue
+++ pages/rollCall/rollCall.vue
@@ -1,15 +1,82 @@
 <template>
-	<view >
-		<image src="../../static/1.png" mode="widthFix" class="a"></image>
-		
+	<view class="a">
+			<view class="ii" v-for="(item,index) in list1" >
+					<img  :src="item.icon" alt="" class="img" >
+					<view class="b">
+						<text class="txt">{{item.text}}</text>
+						<text class="txt1">{{item.text1}}</text>
+					</view>	
+			</view>
 	</view>
+
+
 </template>
 
 <script>
+	export default {
+		data(){
+			return{
+				list1: [{
+						icon: "../../static/xl.png",
+						text: "上海市徐汇区建国西路648号2718室",
+						text1: "2023-07-01 15：23"
+					},
+					{
+						icon: "../../static/xl.png",
+						text: "上海市虹口区水电路609弄-14号-210室",
+						text1: "2023-07-02 08：10"
+					},
+					{
+						icon: "../../static/xl.png",
+						text: "上海市虹口区广灵一路74号",
+						text1: "2023-07-03 10：33"
+					}
+				],
+			}
+		},
+		onLoad() {
+			
+			
+		},
+		methods: {	
+			click(item){
+				uni.navigateTo({
+					url: item
+				});
+			}
+			
+		}
+	}
+
 </script>
 
 <style>
-	.a{
-		width: 750rpx;
+		
+	.ii{
+		display: flex;
+		flex-direction: row;
+		margin-top: 50rpx;
+		height: 130rpx;
+		border-bottom: 1px solid #CDCDCD ;
 	}
+	.img{
+		width: 50rpx;
+		height: 50rpx;
+		margin-left: 50rpx;
+	}
+	.b{
+		display: flex;
+		flex-direction: column;
+	}
+	.txt{
+		margin-left: 50rpx;
+		font-weight: bold;
+	}
+	.txt1{
+		margin-left: 50rpx;
+		margin-top: 20rpx;
+		font-size: 30rpx;
+		color: #CDCDCD ;
+	}
+
 </style>
